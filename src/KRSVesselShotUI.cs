129e3d87e1f0b6e3f4609aafc96b2a4db942c591
unknown <bigorangemachine@hotmail.com>
2015-04-27 19:43:02 -0400

    1.0 Update

--- src/KRSVesselShotUI.cs
+++ src/KRSVesselShotUI.cs
@@ -40,6 +40,14 @@ namespace KronalUtils
             GameEvents.onGUIApplicationLauncherReady.Add(OnGUIAppLauncherReady);
         }
 
+        private void Start()
+        {
+            if (KVVButton == null)
+            {
+                this.OnGUIAppLauncherReady();
+            }
+        }
+
         private void ConfigApplied()
         {
             ButtonMode(false);
